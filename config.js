31049992039e879a221839834e9b63ec58e1044d
YANG XIMING <yxm1010122195@gmail.com>
2023-08-17 04:53:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy.netptop.workers.dev',
+    serverName: 'www.scod.eu.org',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
